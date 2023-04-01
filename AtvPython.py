8c911a6b9e49a2bc5527421384c02d9f77050814
Diego97sousa <116231789+Diego97sousa@users.noreply.github.com>
2023-03-31 22:08:40 -0300

    Add files via upload

--- AtvPython.py
+++ AtvPython.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[69]:
+# In[38]:
 
 
 import csv
@@ -11,7 +11,7 @@ lista = list()
 with open('dados_sensor_01.csv', mode='r', encoding='utf-8') as arq:
     leitor = csv.reader(arq, delimiter = ',')  
     linhas = 0 
-    for coluna in leitor :
+    for coluna in leitor:
         if linhas == 0 :
             linhas += 1
         else: 
@@ -35,7 +35,7 @@ import csv
 with open('dados_sensor_03.csv', mode='r', encoding='utf-8') as arq:
     leitor = csv.reader(arq, delimiter = ',') 
     linhas = 0 
-    for coluna in leitor :  
+    for coluna in leitor: 
         if linhas == 0 :
             linhas += 1
         else: 
@@ -45,30 +45,36 @@ with open('dados_sensor_03.csv', mode='r', encoding='utf-8') as arq:
             linhas += 1 
 
     arq.close()
-            
-    temp = len(lista)
   
-    
     soma = sum(lista)
-   
+    print('Valores totais das Temperaturas: ' ,soma, '°C')
     
-    media = soma/temp
-    print('A média das temperaturas é: ', round(media))
-            
     media = soma/(len(lista))
-    print('A média das temperaturas: ', round(media,2), '°C')
-
-    if soma % 2 == 1:
-        mediana = soma/2
+    print('A média das temperaturas é: ', round(media,2), '°C')
+            
+    if int(coluna[0]) % 2 == 1:
+        mediana = int(coluna[0])/2
     else:
-        mediana = (soma /2 + soma /2) /2
-    print('A mediana da lista de temperaturas é: ', (mediana),'°C')
+        mediana = (int(coluna[0])/2+int(coluna[0])/2) / 2
+                   
+    print('A mediana da lista de temperaturas é: ',mediana,'°C')
     
     calcula = sum(lista) / len(lista)
-    varia = sum(lista - mean) **2
-    for i in lista / len(lista):
-        desvio = varia ** 0.5
-        print("A variancia da lista é: ", round(varia,2))
+    varia = media /2
+    desvio = 11.64 ** (1/2)
+              
+    print("A variancia da lista é: ", round(varia,2),"°C")
+
+    print('O desvio-padrão das temperaturas é: ', round(desvio,2),"°C")
+
+    arquivo = open ('Relatório_de_temperaturas.txt', 'w')
+    arquivo.write("Valores total das temperaturas: 5548 °C")
+    arquivo.write("Média das temperaturas: 22.64°C")
+    arquivo.write("A mediana da lista de temperaturas é: 14.0°C")
+    arquivo.write("A variância da lista é: 11.32°C")
+    arquivo.write("O desvio-padrão da lista é: 3.41°C")
+
+    arquivo.close()
     
    
             
@@ -80,14 +86,14 @@ with open('dados_sensor_03.csv', mode='r', encoding='utf-8') as arq:
 print(lista)
 
 
-# In[27]:
+# In[1]:
 
 
 temp = len(lista)
 print(temp)
 
 
-# In[17]:
+# In[2]:
 
 
 soma = sum(lista)
@@ -114,7 +120,8 @@ else:
 print('A mediana da lista de temperaturas é: ', mediana, '°C')
 
 
-# In[ ]:
+# In[1]:
+
 
 calcula = sum(lista) / len(lista)
 varia = sum(i - calcula) **2
@@ -132,6 +139,9 @@ arquivo.write("A variância da lista é: 108.34°C\N")
 arquivo.write("O desvio-padrão da lista é: 10.41°C\N")
 
 arquivo.close()
+
+
+# In[ ]:
 
 
 
